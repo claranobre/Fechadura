ddeae05d60e972eb49b51e2f402b4630a6217ff1
claranobre <aclaranobre@gmail.com>
2016-06-10 10:38:33 -0300

    QtFechadura atualizado, funcoes de inicializacao, verificacao de senha

--- QtFechadura/main.cpp
+++ QtFechadura/main.cpp
@@ -9,10 +9,15 @@
 #include <iostream>
 
 #define port 4325
+
+using namespace std;
+
 /*
 *Senha
 */
 struct Senha{
+    int entrada = 0;
+    int count = 0;
     int posicao = 0;
     char senha[4] = "2016"; /*Senha*/
     const byte linhas = 4; /*Quatro linhas*/
@@ -40,6 +45,57 @@ void setup(){
 /*
 * Função para verificar a senha
 */
+void loop(){
+    char key = keypad.getKey();                         //Obtém tecla pressionada
+        if (key != NO_KEY){                             //Se foi pressionada uma tecla:
+            if (key == '#') {                           //Se a tecla é '#'
+            code_entry_init();                          //Então espera que seja inserida uma senha
+
+            while (entrada < 4 ){                       //Conta 4 entradas/teclas
+            char key = keypad.getKey();                //Obtém tecla pressionada
+                if (key != NO_KEY){                    //Se foi pressionada uma tecla:
+                entrada += 1;                          //Faz entrada = entrada + 1
+
+                /* tone(audioPin, 1080, 100);          //Para cada dígito emite um som de indicação
+                delay(duration);                       //Duração do som
+                noTone(audioPin); */                   //Para de emitir som
+
+              if (key == senha[posicao])                //Se a tecla pressionada corresponde ao dígito
+                  posicao++;                           //da senha correspondente, soma 1 no contador
+
+              if (posicao == 4 )                        //Se contador chegou a 4 e com dígitos corretos,
+                  fechadoOk(false);                     //desbloqueia sistema
+
+              if ((key == '#') || (entrada == 4)){     //Se foi pressionada a tecla "#' ou foram feitas
+                                                       //4 entradas,
+              key_init();                              //Inicializa o sistema
+              break;                                   //Para o sistema e espera por uma tecla
+              //delay(100);
+              }
+            }
+          }
+        }
+      }
+}
+
+/*void fechadoOk(int fechado){
+  if (fechado){
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+  }
+  else{
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+  }*/
+}
+
+/*
+* Inicializando o sistema
+*/
+
+void key_init(){
+    std::cout << "";
+}
 
 
 int main(int argc, char *argv[])
